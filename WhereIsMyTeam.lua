1633f28b271cf3198c3be134ac23599d2eee08fe
Xie Qiu <qiux0518@hotmail.com>
2021-11-16 10:52:35 -0800

    Update WhereIsMyTeam.lua

--- WhereIsMyTeam.lua
+++ WhereIsMyTeam.lua
@@ -20,7 +20,7 @@ if not UnitInParty(frame.unit) then return end
 if frame:GetName() then return end	-- skip CompactRaidFrame. party nameplate has no name
 		
 if not frame.icon then
-	frame.optionTable.displaySelectionHighlight = false
+	frame.optionTable.displaySelectionHighlight = false	-- otherwise healthbar shows up
 	frame.icon = frame:CreateTexture(nil, "OVERLAY")
 	local t = CLASS_ICON_TCOORDS[select(2, UnitClass(frame.unit))]
 	frame.icon:SetTexture("Interface\\GLUES\\CHARACTERCREATE\\UI-CHARACTERCREATE-CLASSES")
