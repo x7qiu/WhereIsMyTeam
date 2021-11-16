0a54f178ee197f1d8c187acfe8953cba7f5df286
Xie Qiu <qiux0518@hotmail.com>
2021-11-16 10:51:49 -0800

    Update WhereIsMyTeam.lua

--- WhereIsMyTeam.lua
+++ WhereIsMyTeam.lua
@@ -2,11 +2,11 @@ hooksecurefunc("CompactUnitFrame_UpdateName", function(frame)
 
 -- remove icon for non-party members
 if frame.icon and not UnitInParty(frame.unit) then
-	print("trying to remove icon")
 	frame.icon:SetTexture(nil)
 	frame.icon:ClearAllPoints()
 	frame.icon = nil
-	frame.healthBar.SetAlpha(1)
+	frame.healthBar:SetAlpha(1)
+	frame.optionTable.displaySelectionHighlight = true
 end
 
 if not UnitIsPlayer(frame.displayedUnit) then return end
@@ -20,7 +20,7 @@ if not UnitInParty(frame.unit) then return end
 if frame:GetName() then return end	-- skip CompactRaidFrame. party nameplate has no name
 		
 if not frame.icon then
-
+	frame.optionTable.displaySelectionHighlight = false
 	frame.icon = frame:CreateTexture(nil, "OVERLAY")
 	local t = CLASS_ICON_TCOORDS[select(2, UnitClass(frame.unit))]
 	frame.icon:SetTexture("Interface\\GLUES\\CHARACTERCREATE\\UI-CHARACTERCREATE-CLASSES")
